776f474c10650ec97963e72be50d4610476855be
victordoshenko <victor.doshenko@gmail.com>
2025-11-14 12:38:48 +0300

    Task5 (#5)
    * Task5 init
    
    * task5

---
+++ Task5/src/main/java/com/example/User.java
@@ -0,0 +1,48 @@
+package com.example;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    public User() {
+    }
+
+    public User(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
+
