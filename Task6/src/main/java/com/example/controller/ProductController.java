ba4cb27b9cf0bde386c0612b0256eec0c2c0b6ae
Victor <victor.doshenko@gmail.com>
2025-11-21 23:46:24 +0300

    fix: CodeReview comments

--- Task6/src/main/java/com/example/controller/ProductController.java
+++ Task6/src/main/java/com/example/controller/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 public class ProductController {
 
     private final ProductService productService;
